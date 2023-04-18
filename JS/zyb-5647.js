105bb01931c64e4e9af494d71f4e2e3d636d04e4
MachadONLY <rm551570@fiap.com.br>
2023-04-18 11:46:33 -0300

    set time alert

--- JS/zyb-5647.js
+++ JS/zyb-5647.js
@@ -7,34 +7,54 @@
 // console.log(liElement)
 
 //Declaração de arrays
-let nr1 = [1, 2, 3, 4, 5];
-let nr2 = [6, 7, 8, 9, 10];
-//Espalhar o array com spread [ ... ]
-let nr3 = [...nr1,... nr2];
+// let nr1 = [1, 2, 3, 4, 5];
+// let nr2 = [6, 7, 8, 9, 10];
+// //Espalhar o array com spread [ ... ]
+// let nr3 = [...nr1,... nr2];
 
-console.log(`Array-1 ${nr1}`);
-console.log(`Array-2 ${nr2}`);
-console.log(`Array-3 ${nr3}`);
+// console.log(`Array-1 ${nr1}`);
+// console.log(`Array-2 ${nr2}`);
+// console.log(`Array-3 ${nr3}`);
 
-//Listando um array! Técnica Especial
-nr3.forEach((itemDoArray)=>{
-    console.log(`Array-3 sendo listado ${nr3}`);
-})
+// //Listando um array! Técnica Especial
+// nr3.forEach((itemDoArray)=>{
+//     console.log(`Array-3 sendo listado ${nr3}`);
+// })
 
-nr3.map((numero,key)=>{
-    console.log(`${(key+1)} - Itens do array - ${numero}`);
-    numero.forEach((digitos)=>{
-        console.log(digitos)
-    })
-})
+// nr3.map((numero,key)=>{
+//     console.log(`${(key+1)} - Itens do array - ${numero}`);
+//     numero.forEach((digitos)=>{
+//         console.log(digitos)
+//     })
+// })
 
-//Convertendo HTMLCollection m Array de forma indireta
-const liElement = document.getElementsByTagName("li");
-const LiElementArray =  [...liElementHtml];
+// //Convertendo HTMLCollection m Array de forma indireta
+// const liElement = document.getElementsByTagName("li");
+// const LiElementArray =  [...liElementHtml];
 
-LiElementArray.map((item,key)=>{
-    if (item.textContent == "item-39") {
-        console.log(`${key+1} - ${item.textContent = "LOCALIZADO"}`)
-    }
+// LiElementArray.map((item,key)=>{
+//     if (item.textContent == "item-39") {
+//         console.log(`${key+1} - ${item.textContent = "LOCALIZADO"}`)
+//     }
 
-})
+// })
+
+function soma(p1,p2,p3) {
+    nr1 = p1;
+    nr2 = p2;
+
+    return (parseint(nr1) + parseint(nr2) + parseint(p3))
+
+//     console.log(`Resultado : ${parseint(nr1) + parseint(nr2) + parseint(parametro)}`);
+//     console.log(`Este é o parâmetro que foi passado para a função : ${parametro}`);
+ }
+
+
+ //ARROW FUNCTON
+ const mudaCor = ()=>{
+
+    //FUNÇÃO SET-TIME-OUT
+    setTimeout(alert("Executou"),5000);
+ }
+
+ mudaCor();
