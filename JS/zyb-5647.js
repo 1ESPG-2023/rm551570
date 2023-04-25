bac745acca52907e6c09cc6d25c09244ff679b59
MachadONLY <rm551570@fiap.com.br>
2023-04-25 11:49:12 -0300

    alterações no banner

--- JS/zyb-5647.js
+++ JS/zyb-5647.js
@@ -39,22 +39,57 @@
 
 // })
 
-function soma(p1,p2,p3) {
-    nr1 = p1;
-    nr2 = p2;
+// function soma(p1,p2,p3) {
+//     nr1 = p1;
+//     nr2 = p2;
 
-    return (parseint(nr1) + parseint(nr2) + parseint(p3))
-
+//     resultado = parseint(nr1) + parseint(nr2) + parseint(p3)
 //     console.log(`Resultado : ${parseint(nr1) + parseint(nr2) + parseint(parametro)}`);
 //     console.log(`Este é o parâmetro que foi passado para a função : ${parametro}`);
- }
+    
 
 
- //ARROW FUNCTON
- const mudaCor = ()=>{
+// //  //ARROW FUNCTON
+// //  const mudaCor = ()=>{
 
-    //FUNÇÃO SET-TIME-OUT
-    setTimeout(alert("Executou"),5000);
- }
+// //     //FUNÇÃO SET-TIME-OUT
+// //     setTimeout(alert("Executou"),5000);
+// //  }
 
- mudaCor();
+// //  mudaCor();
+
+function mudaCor() {
+    
+    let r = "";
+    let g = "";
+    let b = "";
+
+    //random = Gera números aleatórios entre 0 e 1
+    //ceil, floor e round que arredondam o número para cima, para baixo ou aleatóriamente.
+    
+    //Determinando para a avariável r um valor entre 0 e 255.
+    r = Math.round (Math.random() * 255);
+    g = Math.round (Math.random() * 255);
+    b = Math.round (Math.random() * 255);
+    
+    const cabecalho = document.querySelector(".cabecalho");
+
+    cabecalho.setAttribute("style", `background-color:rgb(${r},${g},${b})`)
+
+    tmp = setTimeout(mudaCor, 2000)
+
+ 
+}
+
+//Função altera banner
+function alteraBanner() {
+
+    let nr = Math.round(Math.random() *3);
+
+    let caminho = `./img/banner-lateral-${nr}.png`;
+
+    const img1 = document.querySelector(".l-e > img")
+    img1.src = caminho;
+}
+
+alteraBanner();
